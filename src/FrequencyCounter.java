173af8fbb3861f33b374b389aa79df7b3b3a6734
zekoliu <zekoliu@foxmail.com>
2018-05-05 09:37:48 +0000

    add 3.3 TestRB and TestBST

--- src/FrequencyCounter.java
+++ src/FrequencyCounter.java
@@ -1,44 +1,68 @@
-import edu.princeton.cs.algs4.Quick;
-import edu.princeton.cs.algs4.ST;
-import edu.princeton.cs.algs4.StdIn;
-import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.*;
+import edu.princeton.cs.algs4.RedBlackBST;
 
 public class FrequencyCounter {
 
     public static void main(String[] args) {
-        int minlen = Integer.parseInt(args[0]);
-        int times = 0;
-        String word = "";
-        ST<String, Integer> st = new ST<String, Integer>();
-        while (!StdIn.isEmpty()) {
-            word = args[0];
-            if (word.length() < minlen) continue;
-            if (!st.contains(word)) st.put(word, 1);
-            else                    st.put(word, st.get(word) + 1);
-            times++;
-        }
+        testST();
+        testBST();
+        testRedBlackBST();
+    }
 
-        StdOut.println("last word is " + word + " times = " + times);
+    public static void testST() {
+        int minlen = 8;
+        Stopwatch stopwatch = new Stopwatch();
+        ST<String, Integer> bst = new ST<String, Integer>();
+        while (!StdIn.isEmpty()) {
+            String word = StdIn.readString();
+            if (word.length() < minlen) continue;;
+            if (!bst.contains(word)) bst.put(word, 1);
+            else                     bst.put(word, bst.get(word) + 1);
+        }
 
         String max = "";
-        st.put(max, 0);
-        for (String anotherWord : st.keys())
-            if (st.get(anotherWord) > st.get(max))
-                max = anotherWord;
+        bst.put(max, 0);
+        for (String word : bst.keys())
+            if (bst.get(word) > bst.get(max))
+                max = word;
+        StdOut.println(max + " " + bst.get(max) + " time: " + stopwatch.elapsedTime());
+    }
 
-        StdOut.print(max + " " + st.get(max));
-
-        int index = 0;
-        String[] keys = new String[st.size()];
-        int[] values = new int[st.size()];
-        for (String s : st.keys()) {
-            keys[index] = s;
-            values[index++] = st.get(s);
+    public static void testBST() {
+        int minlen = 8;
+        Stopwatch stopwatch = new Stopwatch();
+        BST<String, Integer> bst = new BST<String, Integer>();
+        while (!StdIn.isEmpty()) {
+            String word = StdIn.readString();
+            if (word.length() < minlen) continue;;
+            if (!bst.contains(word)) bst.put(word, 1);
+            else                     bst.put(word, bst.get(word) + 1);
         }
 
-        Quick.sort(keys);
-        for (int i = 0; i < keys.length; i++)
-            StdOut.print(keys[i] + "-" + st.get(keys[i]) + "  ");
-        StdOut.println();
+        String max = "";
+        bst.put(max, 0);
+        for (String word : bst.keys())
+            if (bst.get(word) > bst.get(max))
+                max = word;
+        StdOut.println(max + " " + bst.get(max) + " time: " + stopwatch.elapsedTime());
+    }
+
+    private static void testRedBlackBST() {
+        int minlen = 8;
+        Stopwatch stopwatch = new Stopwatch();
+        RedBlackBST<String, Integer> bst = new RedBlackBST<String, Integer>();
+        while (!StdIn.isEmpty()) {
+            String word = StdIn.readString();
+            if (word.length() < minlen) continue;;
+            if (!bst.contains(word)) bst.put(word, 1);
+            else                     bst.put(word, bst.get(word) + 1);
+        }
+
+        String max = "";
+        bst.put(max, 0);
+        for (String word : bst.keys())
+            if (bst.get(word) > bst.get(max))
+                max = word;
+        StdOut.println(max + " " + bst.get(max) + " time: " + stopwatch.elapsedTime());
     }
 }
