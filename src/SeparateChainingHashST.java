7e6e54be3600a45b9173ba8776034d9f1fdd329b
zekoliu <zekoliu@foxmail.com>
2018-05-11 09:14:00 +0000

    add 3.4 SeparateChainingHashST some function

--- src/SeparateChainingHashST.java
+++ src/SeparateChainingHashST.java
@@ -1,3 +1,4 @@
+import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.StdOut;
 
 public class SeparateChainingHashST<Key, Value> {
@@ -27,19 +28,44 @@ public class SeparateChainingHashST<Key, Value> {
     }
 
     private void put(Key key, Value value) {
+        if (N > 2/M)
+            resize(2 * M);
         st[hash(key)].put(key, value);
     }
 
     private void delete(Key key) {
+        if (N > 0 && N < M/8)
+            resize(M/2);
         st[hash(key)].delete(key);
+    }
+
+    public Iterable<Key> keys() {
+        Queue<Key> queue = new Queue<Key>();
+        for (int i = 0; i < M; i++)
+            for (Key key : st[i].keys())
+                queue.enqueue(key);
+        return queue;
+    }
+
+    public void resize(int cap) {
+        SeparateChainingHashST<Key, Value> t = new SeparateChainingHashST<Key, Value>(cap);
+        for (int i = 0; i < M; i++)
+            for (Key key : st[i].keys())
+                t.put(key, st[i].get(key));
+        this.st = t.st;
+        this.M = t.M;
+        this.N = t.N;
     }
 
     public static void main(String[] args) {
         String s = "S E A R C H E X A M P L E";
         String[] strings = s.split(" ");
-        SeparateChainingHashST<String, Integer> st = new SeparateChainingHashST<>(15);
+        SeparateChainingHashST<String, Integer> st = new SeparateChainingHashST<>(11);
         for (int i = 0; i < strings.length; i++)
             st.put(strings[i], i);
         StdOut.println("get A equal " + st.get("A"));
+        st.delete("A");
+        for (String s1 : st.keys())
+            StdOut.print(s1 + " ");
     }
 }
