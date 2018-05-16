12d7e17204a4edb8f75e2cdce403170b89dd3508
zekoliu <zekoliu@foxmail.com>
2018-05-16 15:22:59 +0000

    add 3.5 WhiteFilter

---
+++ src/WhiteFilter.java
@@ -0,0 +1,21 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.HashSet;
+
+public class WhiteFilter {
+
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet<String>();
+        In in = new In(args[0]);
+        while (!in.isEmpty())
+            set.add(in.readString());
+
+        while (!StdIn.isEmpty()) {
+            String word = StdIn.readString();
+            if (set.contains(word))
+                StdOut.print(word + " ");
+        }
+    }
+}
