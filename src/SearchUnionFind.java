9a957f6195c4ef6d20f0f2dbe1f7a78a81656c50
zekoliu <zekoliu@foxmail.com>
2018-05-19 11:26:39 +0000

    add 4.1.8 SearchUF

---
+++ src/SearchUnionFind.java
@@ -0,0 +1,36 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.UF;
+
+public class SearchUnionFind {
+    private UF uf;
+    private int s;
+
+    public SearchUnionFind(Graph G, int s) {
+        uf = new UF(G.V());
+        this.s = s;
+    }
+
+    public boolean marked(int w) {
+        return uf.connected(s, w);
+    }
+
+    public int count() {
+        return uf.count();
+    }
+
+    public static void main(String[] args) {
+        Graph G = new Graph(new In(args[0]));
+        int s = Integer.parseInt(args[1]);
+        SearchUnionFind suf = new SearchUnionFind(G, s);
+
+        for (int v = 0; v < G.V(); v++)
+            if (suf.marked(v))
+                StdOut.print(v + " ");
+        StdOut.println();
+
+        if (suf.count() != G.V())
+            StdOut.print("NOT ");
+        StdOut.println("connected");
+    }
+}
