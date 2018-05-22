adbe19db02f46c49419ced2686ea3f89c782323c
zekoliu <zekoliu@foxmail.com>
2018-05-22 11:15:08 +0000

    add 4.2 Digraph

---
+++ src/Digraph.java
@@ -0,0 +1,68 @@
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Digraph {
+    private final int V;
+    private int E;
+    private Bag<Integer>[] adj;
+
+    public Digraph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<Integer>[]) new Bag[V];
+        for (int v = 0; v < V; v++)
+            adj[v] = new Bag<Integer>();
+    }
+
+    public Digraph(In in) {
+        this(in.readInt());
+        int E = in.readInt();
+        for (int i = 0; i < E; i++) {
+            int v = in.readInt();
+            int w = in.readInt();
+            addEdge(v, w);
+        }
+    }
+
+    public void addEdge(int v, int w) {
+        adj[v].add(w);
+        E++;
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    public Iterable<Integer> adj(int v) {
+        return adj[v];
+    }
+
+    public Digraph reverse() {
+        Digraph R = new Digraph(V);
+        for (int v = 0; v < V; v++)
+            for (int w : adj[v])
+                R.addEdge(w, v);
+        return R;
+    }
+
+    public String toString() {
+        String s = "";
+        for (int i = 0; i < V; i++) {
+            s = s + i + ": ";
+            for (int v : adj[i])
+                s = s + v + " ";
+            s = s + "\n";
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        Digraph d = new Digraph(new In(args[0]));
+        StdOut.println(d.toString());
+    }
+}
