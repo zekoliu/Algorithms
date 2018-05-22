790aa7c7ac9798d2acf87274219c04556df6c9f5
zekoliu <zekoliu@foxmail.com>
2018-05-22 21:31:09 +0000

    add 4.2 DirectedCycle

---
+++ src/DirectedCycle.java
@@ -0,0 +1,55 @@
+import edu.princeton.cs.algs4.Graph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
+
+public class DirectedCycle {
+    private boolean[] marked;
+    private int[] edgeTo;
+    private Stack<Integer> cycle;
+    private boolean[] onStack;
+
+    public DirectedCycle(Digraph G) {
+        onStack = new boolean[G.V()];
+        edgeTo = new int[G.V()];
+        marked = new boolean[G.V()];
+        for (int v = 0; v < G.V(); v++)
+            if (!marked[v])
+                dfs(G, v);
+    }
+
+    public void dfs(Digraph G, int v) {
+        onStack[v] = true;
+        marked[v] = true;
+        for (int w : G.adj(v))
+            if (this.hasCycle()) return;
+            else if (!marked[w]) {
+                edgeTo[w] = v;
+                dfs(G, w);
+            }
+            else if (onStack[w]) {
+                cycle = new Stack<Integer>();
+                for (int x = v; x != w; x = edgeTo[x])
+                    cycle.push(x);
+                cycle.push(w);
+                cycle.push(v);
+            }
+        onStack[v] = false;
+    }
+
+    public boolean hasCycle() {
+        return cycle != null;
+    }
+
+    public Iterable<Integer> cycle() {
+        return cycle;
+    }
+
+    public static void main(String[] args) {
+        Digraph G = new Digraph(new In(args[0]));
+        DirectedCycle Dcycle = new DirectedCycle(G);
+        if (Dcycle.hasCycle())
+            for (int v : Dcycle.cycle())
+                StdOut.print(v + " ");
+    }
+}
