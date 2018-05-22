efe6de3b64950bb8796a20ed41b2bae38a11632b
zekoliu <zekoliu@foxmail.com>
2018-05-22 21:30:06 +0000

    add 4.2 DepthFirstOrder

---
+++ src/DepthFirstOrder.java
@@ -0,0 +1,63 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
+
+public class DepthFirstOrder {
+    private boolean[] marked;
+    private Queue<Integer> pre;
+    private Queue<Integer> post;
+    private Stack<Integer> reversePost;
+
+    public DepthFirstOrder(Digraph G) {
+        pre = new Queue<Integer>();
+        post = new Queue<Integer>();
+        reversePost = new Stack<Integer>();
+        marked = new boolean[G.V()];
+
+        for (int v = 0; v < G.V(); v++)
+            if (!marked[v])
+                dfs(G, v);
+    }
+
+    private void dfs(Digraph G, int v) {
+        pre.enqueue(v);
+
+        marked[v] = true;
+        for (int w : G.adj(v))
+            if (!marked[w])
+                dfs(G, w);
+
+        post.enqueue(v);
+        reversePost.push(v);
+    }
+
+    public Iterable<Integer> pre() {
+        return pre;
+    }
+
+    public Iterable<Integer> post() {
+        return post;
+    }
+
+    public Iterable<Integer> reversePost() {
+        return reversePost;
+    }
+
+    public static void main(String[] args) {
+        Digraph G = new Digraph(new In(args[0]));
+
+        DepthFirstOrder DFO = new DepthFirstOrder(G);
+        for (int v : DFO.pre())
+            StdOut.print(v + " ");
+        StdOut.println();
+
+        for (int lastV : DFO.post())
+            StdOut.print(lastV + " ");
+        StdOut.println();
+
+        for (int reverseV : DFO.reversePost())
+            StdOut.print(reverseV + " ");
+        StdOut.println();
+    }
+}
